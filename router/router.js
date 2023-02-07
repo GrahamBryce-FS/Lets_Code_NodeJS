fb695b2298da1dedc1496edd68ac6c1b1c95ec7b
Bryce Graham <bgraham1@student.fullsail.edu>
2023-02-07 10:40:16 +0900

    cleaned up code

--- router/router.js
+++ router/router.js
@@ -10,10 +10,10 @@ res.status(200).json({
         method: req.method,
         },
     });
-})
+});
 
-// http://localhost:3000/exercise
-router.get("/:id", (req,res,next)=>{
+// http://localhost:3000/exercise/get/45
+router.get("/get/:id", (req,res,next)=>{
     const id = req.params.id
     res.status(200).json({
         message: 'Successful - GET',
@@ -21,11 +21,11 @@ router.get("/:id", (req,res,next)=>{
         metadata:{
             hostname: req.hostname,
             method: req.method,
-        }    
-    })
-})
+        },   
+    });
+});
 
-// patch
+// http://localhost:3000/exercise/patch/89
 router.get("/patch/:id", (req,res)=>{
     const id = req.params.id;
     res.status(200).json({
@@ -37,17 +37,26 @@ router.get("/patch/:id", (req,res)=>{
         },
     });
 });
-// post
+// http://localhost:3000/exercise/post
 router.get("/post", (req,res)=>{
     res.status(200).json({
-        message: "Using Post status 201",
+        message: "Using Post",
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+        },
     });
 });
-// delete
+// http://localhost:3000/exercise/delete/9
 router.get("/delete/:id", (req,res)=>{
     const id = req.params.id;
     res.status(200).json({
+        id: id,
         message: `deleted user ${id}`,
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+        },
     });
 });
 
